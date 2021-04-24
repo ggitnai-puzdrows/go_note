19a2cecfb9717c0a972828a5d39fd9ec82884241
ggitnai-puzdrows <igapuzdrowska@gmail.com>
2021-04-24 14:58:07 +0200

    Small gui updates

--- src/App.vue
+++ src/App.vue
@@ -88,7 +88,7 @@
 
 <style>
 	.cover {
-	background-image: url("./assets/background2.jpg");
+	background-image: url("./assets/background2.jpeg");
 	background-position: center;
 	background-size: cover;
 	min-height: 150px;
